2dc7c8d567ac57aa5271a9c85a6b31371b9c3792
Albin Baby <albinbabayme05@gmail.com>
2024-12-23 11:54:18 +0100

    modifcation for app setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+
 import './App.css'
+import NavBar from './Components/NavBar/NavBar'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div>netflix app</div>
+    <div className='App'>
+      <NavBar />
+    </div>
   )
 }
 
