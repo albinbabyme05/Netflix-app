0684c535bbb5c3faacb767d1fb1ef05f19052151
Albin Baby <albinbabayme05@gmail.com>
2024-12-26 14:07:28 +0100

    banner modified by adding new movies dynamicaly using axios api from TMDB  moveie database

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,15 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "./Banner.css"
+import axios  from '../../axios'
+import { API_KEY, IMAGE_URL } from '../../constants/constants'
+
+
+
 
 function Banner() {
+
+  /* usestatet to set the movies */
+  const [movie, setmovie] = useState()
+
+  /* from the firstime of mountuíng getting all data */
+
+  useEffect(() => {
+    axios.get(`trending/all/week?api_key=${API_KEY}&language=en-US`)
+    .then((response)=>{
+      console.log(response.data); 
+      setmovie(response.data.results[1])
+    })
+  }, [])
+
+
   return (
-    <div className='banner'>
+    <div 
+    style={{backgroundImage: `url(${movie ? IMAGE_URL + movie.backdrop_path : " "})`}}
+    className='banner'>
         <div className='content'>
-            <h1 className='title'>Movie Name</h1>
+            <h1 className='title'>{movie ? movie.title : " "}</h1>
             <div className='banner_buttons'>
                 <button className='button'>Play</button>
                 <button className='button'>My list</button>
-                <h1 className='description'>This page is dedicated for netflix but under construction</h1>
+                <h1 className='description'>{movie ? movie.overview : ""}</h1>
             </div>
         </div>
         <div className="fade_bottom"></div>
